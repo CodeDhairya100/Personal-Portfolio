3958af70263ae5562f3df35e36577779216601dc
CoderDhairya100 <dhairyaprofilo@gmail.com>
2025-02-07 16:21:51 +0530

    Fifth commit

---
+++ portfolio.js
@@ -0,0 +1,44 @@
+// Handling Tab Switching in About Section
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Handling Sidebar Menu Open/Close
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+// Handling Form Submission (Optional: Can Be Used to Send Data to Google Sheets or Email)
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    alert("Thank you, " + name + "! Your message has been sent.");
+    
+    // Reset form after submission
+    document.getElementById("myForm").reset();
+});
